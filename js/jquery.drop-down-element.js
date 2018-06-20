444bdf544e0b75684d967450be23ee71d9d5d0d7
gassrobertpaul <gassrobertpaul@yahoo.com>
2018-06-20 15:18:30 -0700

    Apply icon color setting.

--- js/jquery.drop-down-element.js
+++ js/jquery.drop-down-element.js
@@ -1,10 +1,18 @@
 (function($) {
-	$.fn.droppedDownElement = function() {
+	$.fn.droppedDownElement = function(options) {
+
+		// Obtain the default setttings to apply to elements.
+		var settings = $.extend({
+			iconColor: "#000000"
+		}, options);
 
 		return this.each(function() {
 
 			// Prepend + icon to elements marked with a class of elementForDropDown
 			$(this).prepend('<span class="drop-down-element dropdownelem-plus"></span>');
+
+			// Apply settings color to + icon.
+			$('.dropdownelem-plus').css('color', settings.iconColor);
 
 		})
 	}
