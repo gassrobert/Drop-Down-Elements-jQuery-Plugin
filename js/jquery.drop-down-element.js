78aba0db1d2f0faad6cbf0b88e9c42baff13c486
gassrobertpaul <gassrobertpaul@yahoo.com>
2018-06-20 15:24:48 -0700

    Add click event for + and - icons to switch back and forth.

--- js/jquery.drop-down-element.js
+++ js/jquery.drop-down-element.js
@@ -14,6 +14,34 @@
 			// Apply settings color to + icon.
 			$('.dropdownelem-plus').css('color', settings.iconColor);
 
+			// If the user clicks on the icon or its parent, check if the icon is a + or a -
+			$(this).on( "click", function(event) {
+
+				// Get the full class of elements with the drop-down-element class
+				var elemClass = $(this).find('.drop-down-element').attr("class");
+
+				// Check if the icon area clicked is a + or a -
+				if (elemClass == "drop-down-element dropdownelem-plus") {
+
+					// Replace the + icon with a -
+					$(this).find( "span.dropdownelem-plus" ).replaceWith( '<span class="drop-down-element dropdownelem-minus"></span>' );
+
+					// Apply the color settings to the - icon
+					$('.dropdownelem-minus').css('color', settings.iconColor);
+
+				} else if (elemClass == "drop-down-element dropdownelem-minus") {
+
+					// Replace the - icon with a +
+					$(this).find( "span.dropdownelem-minus" ).replaceWith( '<span class="drop-down-element dropdownelem-plus"></span>' );
+
+					// Apply the color settings to the + icon
+					$('.dropdownelem-plus').css('color', settings.iconColor);
+
+				}					
+
+
+			}); // End of $(this).on( "click", function(event) {
+
 		})
 	}
 }(jQuery));
